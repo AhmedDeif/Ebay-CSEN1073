c8b4906903d14dd26bf145701c30cdf75c350b85
Doola <ahmed.adel.abodeif@gmail.com>
2017-04-13 17:56:21 +0200

    Adding Merchant App package

---
+++ src/MerchantApp/ParseListener.java
@@ -0,0 +1,9 @@
+package MerchantApp;
+public interface ParseListener{
+
+    public abstract void parsingFinished(  ClientHandle  clientHandle, 
+                                           ClientRequest clientRequest );
+
+    public abstract void parsingFailed( ClientHandle clientHandle, String strError );
+    
+}
